916b891027cb75f4e1d7c22cdf7ab694d4d5d322
Matthew Wang <matthewang.wang@gmail.com>
2017-07-30 22:08:55 -0400

    updates overview for war to contain actual information

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$('#static-content').find('h2').each(function() {
+	$('div#static-content').find("h2").each(function() {
 		prevTitle = $('#sidebar-menu .main-menu').find('#' + $("h1").attr('id') + '-menu');
 		prevTitle.not(":has(ul)").append('<ul class="sub-menu"></ul>');
 		prevTitle.find('.sub-menu').append('<li id="'+ $(this).attr('id') + '-menu"><a href="#' + $(this).attr('id') + '">' + $(this).html() + '</li>');
